480ca66df9c84d5df3d7ce37da8ef1204dbea6ed
Will Reppun <reppun@gmail.com>
2017-06-09 16:33:08 -1000

    wip

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -2,10 +2,11 @@
 const {db} = require('../db.js');
 const getPks = require('./gamePks.js').onDate;
 const {exitVelocities, leaguePerformance} = require('./statcastData.js');
-const {flatten, nextDay} = require('../utils.js');
-const timeout = 30000;
+const {flatten, nextDay, peek} = require('../utils.js');
+const evTimeout = 30000;
+const pkTimeout = 1000;
 
-const singleDay = date => {
+const evSingleDay = date => {
   return getPks(date)
     .map(exitVelocities)
     .then(flatten)
@@ -13,17 +14,19 @@ const singleDay = date => {
     .then(ids => console.log(ids[ids.length - 1]));
 };
 
-const range = (current, end) => {
-  singleDay(current)
-    .then(() => {
-      const nextDate = nextDay(current);
+const forDateRange = (forDateFn, timeout) => {
+  return function range (current, end) {
+    forDateFn(current)
+      .then(() => {
+        const nextDate = nextDay(current);
 
-      if (nextDate.getTime() > end.getTime()) {
-        return;
-      }
+        if (nextDate.getTime() > end.getTime()) {
+          return;
+        }
 
-      setTimeout(() => range(nextDate, end), timeout);
-    });
+        setTimeout(() => range(nextDate, end), timeout);
+      });
+  };
 };
 
 const speedRange = (low, high) => {
@@ -35,17 +38,36 @@ const speedRange = (low, high) => {
         return;
       }
 
-      setTimeout(() => speedRange(low + 1, high), timeout / 10);
+      setTimeout(() => speedRange(low + 1, high), evTimeout / 10);
     });
+};
+
+const gamePksRange = (start, end) => {
+  console.log(start, end);
+
+  const getPksRange = forDateRange(current => {
+    return getPks(current)
+      .then(peek)
+      .map(gamePk => db('game_pk').insert({
+        id: gamePk,
+        game_date: current.toISOString()
+      }));
+  }, pkTimeout);
+
+  getPksRange(start, end);
 };
 
 module.exports = {
   exitVelocities: {
-    singleDay,
-    range
+    singleDay: evSingleDay,
+    range: forDateRange(evSingleDay, evTimeout)
   },
   leaguePerformance: {
     singleSpeed: leaguePerformance,
     speedRange
-  }
+  },
+  gamePks: {
+    range: gamePksRange
+  },
+  test: () => console.log('hi?')
 };
